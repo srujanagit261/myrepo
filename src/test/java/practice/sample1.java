44d00b5c6548b93c799666fea6c288810030459b
apitesting263git <126392395+apitesting263git@users.noreply.github.com>
2023-02-26 18:22:01 +0530

    Revert "second change in my test"

--- src/test/java/practice/sample1.java
+++ src/test/java/practice/sample1.java
@@ -1,11 +1,10 @@
 package practice;
 
-public class Sample1 {
+public class sample1 {
 
 	public static void main(String[] args)
 	{
 		System.out.println("hi tej");
-		System.out.println("hellooooo ");
 
 	}
 
